3b1f7e4bbd9a7f9918e74499f8b9cb3ed7336b6b
marco-gulino <marcogulino98@gmail.com>
2025-02-06 15:01:28 +0100

    [feat] created "controllers" folder and "postController.js" file

---
+++ controllers/postController.js
@@ -0,0 +1,29 @@
+//INDEX
+function index(req, res){
+    res.send("Stampo tutti i post");
+}
+
+//SHOW
+function show(req, res){
+    res.send("Stampo i post con id: " + req.params.id);
+}
+
+//STORE
+function store(req, res){
+    res.send("Aggiungo un post");
+}
+
+//UPDATE
+function update(req, res){
+    res.send("Modifico interamente il post con id: " + req.params.id);
+}
+
+//MODIFY
+function modify(req, res){
+    res.send("Modifico parzialmente il post con id: " + req.params.id);
+}
+
+//DESTROY
+function destroy(req, res){
+    res.send("Cancello il post con id: " + req.params.id);
+}
